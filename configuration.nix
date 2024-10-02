4dbd1a0c47e30257ef1fc8fb44a01967ee840c52
williameliasson-dev <williameliasson5@gmail.com>
2024-10-02 19:33:47 +0200

    remove hyrland from conf

--- configuration.nix
+++ configuration.nix
@@ -77,8 +77,6 @@
   # Enable the X11 windowing system.
   #services.xserver.enable = true;
 
- programs.hyprland.enable = true;
-
   # Configure keymap in X11
   services.xserver = {
     layout = "se";
