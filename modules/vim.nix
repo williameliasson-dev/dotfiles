7e5f2499d2d5ccee67c61febab567f8b01f25c2d
williameliasson-dev <williameliasson5@gmail.com>
2024-10-09 18:18:36 +0200

    config nixvim etc.. (formatting soon lol)

--- modules/vim.nix
+++ modules/vim.nix
@@ -4,5 +4,29 @@
 programs.nixvim = {
   enable = true;
   defaultEditor = true;
+  colorschemes.gruvbox.enable = true;
+  globals.mapleader = " ";
+
+  plugins = {
+   telescope = {
+	enable = true;
+	keymaps = {
+
+
+   
+
+
+  "<leader>ff" = {
+    action = "find_files";
+    options = {
+      desc = "Telescope Files";
+    };
+  };
+  "<leader>fg" = "live_grep";
+};
+
+   };
+  };
+
 };
 }
