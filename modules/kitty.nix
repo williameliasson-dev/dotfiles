a8454c8d3bb7a2611eaa611360b1b83f0f8442e4
williameliasson-dev <williameliasson5@gmail.com>
2024-10-14 21:45:45 +0200

    kitty gruvbox theme

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -35,6 +35,54 @@
 
       # For example, complex key mappings:
       map ctrl+shift+d new_tab_with_cwd
+
+      # Gruvbox Dark color scheme
+
+      # The basic colors
+      foreground              #ebdbb2
+      background              #282828
+      selection_foreground    #655b53
+      selection_background    #ebdbb2
+
+      # Cursor colors
+      cursor                  #8ec07c
+      cursor_text_color       #1d2021
+
+      # URL underline color when hovering with mouse
+      url_color               #d65d0e
+
+      # The basic 16 colors
+      # black
+      color0                  #282828
+      color8                  #928374
+
+      # red
+      color1                  #cc241d
+      color9                  #fb4934
+
+      # green
+      color2                  #98971a
+      color10                 #b8bb26
+
+      # yellow
+      color3                  #d79921
+      color11                 #fabd2f
+
+      # blue
+      color4                  #458588
+      color12                 #83a598
+
+      # magenta
+      color5                  #b16286
+      color13                 #d3869b
+
+      # cyan
+      color6                  #689d6a
+      color14                 #8ec07c
+
+      # white
+      color7                  #a89984
+      color15                 #ebdbb2
     '';
   };
 }
