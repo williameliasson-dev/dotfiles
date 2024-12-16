6058f077c343287b054f79ea5fef1bdee446a4b6
williameliasson-dev <williameliasson5@gmail.com>
2024-12-16 21:41:02 +0100

    setup tuigreet

--- configuration.nix
+++ configuration.nix
@@ -107,8 +107,8 @@
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.hyprland}/bin/Hyprland";
-          user = "william";
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd ${pkgs.hyprland}/bin/Hyprland";
+          user = "greeter";
         };
       };
     };
